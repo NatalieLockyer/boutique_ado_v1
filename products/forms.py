9513ef774ff0e9bd203d49574b90470d7d2ffe6b
Natalie Lockyer <Natalie_lockyer@outlook.com>
2024-05-16 10:48:55 +0000

    Product admin, add product view, URL and template

--- products/forms.py
+++ products/forms.py
@@ -2,7 +2,7 @@ from django import forms
 from .models import Product, Category
 
 
-class Product_form(forms.ModelfForm):
+class ProductForm(forms.ModelForm):
 
     class Meta:
         model = Product
